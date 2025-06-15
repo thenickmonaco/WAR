aef2da6f3d956c3da630fc989a09a1bf0290f44d
Nick Monaco <thenickmonaco@gmail.com>
2025-06-15 06:29:39 -0400

    update src/render.rs (2 total lines changed)

--- src/render.rs
+++ src/render.rs
@@ -15,7 +15,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 //=============================================================================
-// src/render.rslfjdks
+// src/render.rs
 //=============================================================================
 
 use glfw::{Context, WindowHint, WindowMode};

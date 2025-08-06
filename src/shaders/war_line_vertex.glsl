83f18ce0beb7c6314de05c484959c76085b65797
Nick Monaco <thenickmonaco@gmail.com>
2025-08-05 23:49:46 -0400

    line shader

---
+++ src/shaders/war_line_vertex.glsl
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------------
+//
+// WAR - make music with vim motions
+// Copyright (C) 2025 Nick Monaco
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// src/shaders/war_line_vertex.glsl
+//-----------------------------------------------------------------------------
+
+#version 450
+
+layout(location = 0) in vec2 inPos;
+layout(location = 1) in vec4 inColor;
+
+layout(push_constant) uniform PushConstants {
+    float zoom;
+    vec2 pan;
+} pc;
+
+layout(location = 0) out vec4 fragColor;
+
+void main() {
+    vec2 pos = inPos * pc.zoom + pc.pan;
+    gl_Position = vec4(pos, 0.0, 1.0);
+    fragColor = inColor;
+}
